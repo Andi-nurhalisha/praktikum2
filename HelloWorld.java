83e2e05dfeaa8e1d2b86fcba5e436aa466e7c292
Andi-nurhalisha <andinurhalisha122@gmail.com>
2020-09-22 23:08:01 +0800

    belajar push

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+	     System.out.println("Hello Icha!");
+		 System.out.println("NAMA:Andi Nur Halisha");
+		 System.out.println("NIM:6706204035");
+		 System.out.println("KLS: D3IF4401");
+	}
+}
